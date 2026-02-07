ca9acf4845b05eb7ce3cdedf5051ce7e3f627168
Mrbrandedssr <ssrajput7474@gmail.com>
2026-02-07 22:16:22 +0530

    Add rotate array solution

---
+++ src/easy/arrays/RotateArray.java
@@ -0,0 +1,33 @@
+package easy.arrays;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void rotateArray(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+
+    }
+
+    private static void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int temp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = temp;
+            l++;
+            r--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 3;
+        rotateArray(nums, k);
+        System.out.println(Arrays.toString(nums));
+
+    }
+
+}
