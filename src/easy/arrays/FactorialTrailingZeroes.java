e8d8357bddbec1926143234a85c46f776c85a221
Mrbrandedssr <ssrajput7474@gmail.com>
2026-02-12 16:17:27 +0530

    Add Factorial Trailing Zeroes Solution

---
+++ src/easy/arrays/FactorialTrailingZeroes.java
@@ -0,0 +1,30 @@
+package easy.arrays;
+
+import java.util.Scanner;
+
+public class FactorialTrailingZeroes {
+
+    public static int trailingZeroes(int n) {
+        int count = 0;
+
+        while (n > 0) {
+            n = n / 5;
+            count = count + n;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int result = trailingZeroes(n);
+
+        System.out.println("Trailing zeroes in " + n + "! = " + result);
+
+        sc.close();
+    }
+}
