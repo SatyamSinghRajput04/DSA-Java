604bff76dc580a6177d001114fd69be641b85458
Mrbrandedssr <ssrajput7474@gmail.com>
2026-02-17 10:32:43 +0530

    Add Remove Linked List Elements Solution

---
+++ src/easy/arrays/RemoveLinkedListElements.java
@@ -0,0 +1,68 @@
+package easy.arrays;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class RemoveLinkedListElements {
+
+    // Remove all nodes with given value
+    public static ListNode removeElements(ListNode head, int val) {
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode prev = dummy;
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            if (curr.val == val) {
+                prev.next = curr.next; // delete node
+            } else {
+                prev = curr; // move prev
+            }
+
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+
+    // Helper method to print linked list
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        // Creating list: 1 -> 2 -> 6 -> 3 -> 4 -> 5 -> 6
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(6);
+        head.next.next.next = new ListNode(3);
+        head.next.next.next.next = new ListNode(4);
+        head.next.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next.next = new ListNode(6);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        int val = 6;
+
+        head = removeElements(head, val);
+
+        System.out.println("After Removing " + val + ":");
+        printList(head);
+    }
+}
