e69b3f4b744a62febf2d683167188e427ef8eada
Mrbrandedssr <ssrajput7474@gmail.com>
2026-03-05 15:43:22 +0530

    AddDelete Node In A Linked List Soluotion

---
+++ src/easy/linkedlist/DeleteNodeInALinkedList.java
@@ -0,0 +1,48 @@
+package easy.linkedlist;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class DeleteNodeInALinkedList {
+
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(5);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(9);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        ListNode nodeToDelete = head.next;
+
+        deleteNode(nodeToDelete);
+
+        System.out.println("After Deleting Node:");
+        printList(head);
+    }
+}
